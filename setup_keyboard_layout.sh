92a004bff46625e5b620bd700b47038f00ef7f5d
Axel Ulmestig <axel.ulmestig@gmail.com>
2023-03-04 20:35:33 +0100

    automate keyboard layout setup

---
+++ setup_keyboard_layout.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo cp se_keyboard_layout /usr/share/X11/xkb/symbols/us_se
+
+echo 'Now go to Settings -> Region & Language, add a new "input source", choose "Swedish (US, with Swedish letters)"'
