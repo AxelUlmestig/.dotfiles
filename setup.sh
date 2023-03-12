9299fb3fa1f50317d48f1775bfe930b2126b0edf
Axel Ulmestig <axel.ulmestig@gmail.com>
2023-03-12 11:50:42 +0100

    merge keyboard layout setup with setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@ DIR=$( cd $( dirname $BASH_SOURCE[0] ) && pwd )
 BASHRC=$HOME/.bashrc
 
 NVIM_HOME=$HOME/.config/nvim
-# VIM_PLUG_LOCATION=$NVIM_RC/plug.vim
 VIM_PLUG_LOCATION="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
 NVIM_RC=$NVIM_HOME/init.vim
 VIM_RC=$HOME/.vimrc
@@ -10,6 +9,8 @@ VIM_RC=$HOME/.vimrc
 GHCI=$HOME/.ghci
 
 SSH_CONFIG=$HOME/.ssh/config
+
+US_SE_KEYBOARD_LAYOUT=/usr/share/X11/xkb/symbols/us_se
 
 rm -f $BASHRC
 ln -s $DIR/bashrc $BASHRC
@@ -34,3 +35,12 @@ if [ ! -f "$VIM_PLUG_LOCATION" ]; then
 else
   echo "Vim plug already found, skipping download"
 fi
+
+# If the keyboard layout was updated for the first time, go to Settings ->
+# Region & Language, add a new "input source", choose "Swedish (US, with
+# Swedish letters)"
+if [ "$(uname -s)" = "Linux" ]; then
+  sudo rm -f $US_SE_KEYBOARD_LAYOUT
+  sudo ln -s $DIR/se_keyboard_layout $US_SE_KEYBOARD_LAYOUT
+fi
+
