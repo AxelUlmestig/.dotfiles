bb05aa8a01bf60439eb6ae06535faee18f5cf1a5
Axel Ulmestig <axel.ulmestig@gmail.com>
2023-03-12 11:31:44 +0100

    automate vim plug setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 DIR=$( cd $( dirname $BASH_SOURCE[0] ) && pwd )
 BASHRC=$HOME/.bashrc
+
+NVIM_HOME=$HOME/.config/nvim
+# VIM_PLUG_LOCATION=$NVIM_RC/plug.vim
+VIM_PLUG_LOCATION="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
+NVIM_RC=$NVIM_HOME/init.vim
 VIM_RC=$HOME/.vimrc
-NVIM_RC=$HOME/.config/nvim/init.vim
+
 GHCI=$HOME/.ghci
+
 SSH_CONFIG=$HOME/.ssh/config
 
 rm -f $BASHRC
@@ -21,3 +27,10 @@ ln -s $DIR/ghci $GHCI
 mkdir -p $HOME/.ssh
 rm -f $SSH_CONFIG
 ln -s $DIR/ssh-config $SSH_CONFIG
+
+if [ ! -f "$VIM_PLUG_LOCATION" ]; then
+  sh -c "curl -fLo \"$VIM_PLUG_LOCATION\" \
+    --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+else
+  echo "Vim plug already found, skipping download"
+fi
